fc6580e2373057bf132a354d9ed1389f22570c0a
VictorElisiario <victorelisiario2@hotmail.com>
2020-09-13 16:15:01 -0300

    Implementado as funcoes do input

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ let numero01;
 let numero02;
 let operador;
 let resultado;
+var palpitefoco = document.querySelector("#input");
 
-function sorteiaOperador () {
-    operador = Math.floor(Math.random() * 4);
-}
 
 function sorteiaNumerosSomaSubtracao () {
     numeroProvavel01 = Math.round(Math.random() * 1000);
@@ -22,73 +20,107 @@ function sorteiaNumerosDivisao () {
     numeroProvavel02 = Math.round(Math.random() * 10);
 }
 
+function sorteiaNumeros () {
+    operador = Math.floor(Math.random() * 4);
 
-sorteiaOperador();
-
-if (operador == 0) {
-    sorteiaNumerosSomaSubtracao ();
-    numero01 = numeroProvavel01;
-    numero02 = numeroProvavel02;
-    document.getElementById('operador').innerHTML = "+";
-    document.getElementById('valor01').innerHTML = numero01;
-    document.getElementById('valor02').innerHTML = numero02;
-    resultado = (numero01 + numero02);
-
-
-} else if (operador == 1) {
-    sorteiaNumerosSomaSubtracao ();
-    if (numeroProvavel01 > numeroProvavel02) {
+    if (operador == 0) {
+        sorteiaNumerosSomaSubtracao ();
         numero01 = numeroProvavel01;
         numero02 = numeroProvavel02;
-    } else {
-        numero01 = numeroProvavel02;
-        numero02 = numeroProvavel01;
+        document.getElementById('operador').innerHTML = "+";
+        document.getElementById('valor01').innerHTML = numero01;
+        document.getElementById('valor02').innerHTML = numero02;
+        resultado = (numero01 + numero02);
+    
+    
+    } else if (operador == 1) {
+        sorteiaNumerosSomaSubtracao ();
+        if (numeroProvavel01 > numeroProvavel02) {
+            numero01 = numeroProvavel01;
+            numero02 = numeroProvavel02;
+        } else {
+            numero01 = numeroProvavel02;
+            numero02 = numeroProvavel01;
+        }
+        document.getElementById('operador').innerHTML = "+";
+        document.getElementById('valor01').innerHTML = numero01;
+        document.getElementById('valor02').innerHTML = numero02;
+        resultado = (numero01 + numero02);
+    
+    
+    } else if (operador == 2) {
+        sorteiaNumerosMultiplicacao();
+        numero01 = numeroProvavel01;
+        numero02 = numeroProvavel02;
+        document.getElementById('operador').innerHTML = "X";
+        document.getElementById('valor01').innerHTML = numero01;
+        document.getElementById('valor02').innerHTML = numero02;
+        resultado = (numero01 * numero02);
+    
+    
+    }else {
+        sorteiaNumerosDivisao ();
+    
+        if (numeroProvavel01 == 0 ) {
+            numeroProvavel01 += 1;       
+        }
+    
+        if (numeroProvavel02 == 0) {
+            numeroProvavel02 += 1;
+        }
+    
+        const numerador = (numeroProvavel01 * numeroProvavel02);
+            numero02 = (numerador / numeroProvavel01);
+        
+            numero01 = numerador;
+            numero02 = numeroProvavel02
+        
+            document.getElementById('operador').innerHTML = "÷";
+            document.getElementById('valor01').innerHTML = numero01;
+            document.getElementById('valor02').innerHTML = numero02;
+            resultado = (numeroProvavel01);   
     }
-    document.getElementById('operador').innerHTML = "+";
-    document.getElementById('valor01').innerHTML = numero01;
-    document.getElementById('valor02').innerHTML = numero02;
-    resultado = (numero01 + numero02);
-
-
-} else if (operador == 2) {
-    sorteiaNumerosMultiplicacao();
-    numero01 = numeroProvavel01;
-    numero02 = numeroProvavel02;
-    document.getElementById('operador').innerHTML = "X";
-    document.getElementById('valor01').innerHTML = numero01;
-    document.getElementById('valor02').innerHTML = numero02;
-    resultado = (numero01 * numero02);
-
-
-}else {
-    sorteiaNumerosDivisao ();
-    const numerador = (numeroProvavel01 * numeroProvavel02);
-    numero02 = (numerador / numeroProvavel01);
-
-    numero01 = numerador;
-    numero02 = numeroProvavel02
-
-    document.getElementById('operador').innerHTML = "÷";
-    document.getElementById('valor01').innerHTML = numero01;
-    document.getElementById('valor02').innerHTML = numero02;
-    resultado = (numeroProvavel01);
+    console.log(numero01);
+    console.log(numero02);
+    console.log(resultado);
+    console.log("----");
 }
 
-console.log(numero01);
-console.log(numero02);
-console.log(resultado);
+function verificacao () {
+
+    var palpite = document.getElementById('input').value;
+    
+    console.log(palpite);
+    console.log(resultado);
+
+    if (resultado == palpite) {
+        console.log("ACERTOU");       
+        
+    } else {
+        console.log("ERROU");
+    }
+
+    
+    sorteiaNumeros();
+    palpite = document.getElementById('input').value = "";
+    palpitefoco.focus();
 
 
-var botaoTentar = document.querySelector("button#tentar");
+
+}
+
+function desistencia () {
+    sorteiaNumeros ();
+}
+
+sorteiaNumeros();
+palpitefoco.focus();
 
 
-
-// var palpite = document.querySelector("input");
-// console.log(palpite);
 
 // palpite.focus();
 //sorteiaNumeros()
-//var botaoIr = document.querySelector("button#ir");
-//botaoIr.onclick = verificacao;
+var botaoTentar = document.querySelector("button#tentar");
+botaoTentar.onclick = verificacao;
 //var botaoAtualizar = document.querySelector("button#atualizar");
 //botaoAtualizar.onclick = resortear;
